1aeceb15ca2b9995543befdf8b8abbea53738ae6
Carla Tresandí <tresandic@gmail.com>
2025-05-12 09:26:11 +0200

    Create Programa_1.py

---
+++ programas/Programa_1.py
@@ -0,0 +1,21 @@
+
+
+
+from microbit import*
+import neopixel
+
+np = neopixel.NeoPixel(pin13, 1)
+rele = pin16 
+
+while True:
+    temp = temperature()
+
+    if temp > 24:
+        np[0] = (0, 255, 0)
+        rele.write_digital(1)
+    else:
+        np[0] = (255, 0, 0)
+        rele.write_digital(0)
+
+  np.show()
+  sleep(1000)
