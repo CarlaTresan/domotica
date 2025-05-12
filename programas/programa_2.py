09aaf7db40afb88a17a960911c194b8e432c4f1b
Carla Tresandí <tresandic@gmail.com>
2025-05-12 09:40:19 +0200

    Update programa_2.py

--- programas/programa_2.py
+++ programas/programa_2.py
@@ -1,3 +1,16 @@
 """Programa que acende o neopixel se a temperatura super os 28º
 Autora: Carla Tresandí Otero
 Data: 05/05/2025"""
+
+from microbit import *
+
+led = pin14
+while True:
+    luz = pin1.read_analog      #leer valor de luz de LDR
+    
+    if luz < 300: 
+        led.write_digital(1)    # Encender LED noche
+    else:
+        led.write_digital(0)    #apagar LED día
+        
+    sleep(1000)
