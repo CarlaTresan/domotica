df6f1feb7daacf8344b122257a8e09875e5e5e90
Carla Tresandí <tresandic@gmail.com>
2025-05-12 09:48:33 +0200

    Update programa_3.py

--- programas/programa_3.py
+++ programas/programa_3.py
@@ -1,1 +1,9 @@
+from microbit import *
+import music
 
+while True:
+    if button_a.was_pressed(): 
+        pin14.write_digital(1)
+        music.play(music.RINGTONE)
+        sleep(5000)
+        pin14.write_digital(0)
