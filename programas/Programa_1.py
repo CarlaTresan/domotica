e98750522aeebf568b83edc7fcc22419813ef156
Carla Tresandí <tresandic@gmail.com>
2025-05-12 09:33:35 +0200

    Update Programa_1.py

--- programas/Programa_1.py
+++ programas/Programa_1.py
@@ -1,4 +1,6 @@
-
+"""Programa que acende o neopixel se a temperatura super os 28º
+Autora: Carla Tresandí Otero
+Data: 05/05/2025"""
 
 
 from microbit import *
@@ -10,11 +12,11 @@ rele = pin16
 while True:
     temp = temperature()
 
-    if temp > 24:
+    if temp > 28:
         np[0] = (0, 255, 0)
         rele.write_digital(1)
     else:
-        np[0] = (255, 0, 0)
+        np[0] = (0, 0, 0)
         rele.write_digital(0)
 
 np.show()
